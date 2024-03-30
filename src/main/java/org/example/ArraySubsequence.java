b324eb4f3d02fcf5d9eb45aebf9c588c26f7acfb
saurabhsrivastava2009 <saurabh.srivastava2009@gmail.com>
2024-03-30 22:13:37 +0530

    pushing missing code

--- src/main/java/org/example/ArraySubsequence.java
+++ src/main/java/org/example/ArraySubsequence.java
@@ -1,2 +1,47 @@
-package org.example;public class ArraySubsequence {
+package org.example;
+
+import java.util.*;
+
+/**
+ * Given we have two arrays as below
+ * int[] arr1 = {5,4,8,6,9,2,1,6,9,4}
+ * int[] arr2 = {6,4,5,9}
+ * We need to print the output in a way, such that
+ * 1 - the element from arr2 must be printed based on the count of the same element in arr1.
+ * 2 - if not present then print the rest of elements from arr1 maintaining the ascending order
+ *
+ * Output - 6644599128
+ */
+public class ArraySubsequence {
+    public static void main(String[] args) {
+        int[] arr1 = {5,4,8,6,9,2,1,6,9,4};
+        int[] arr2 = {6,4,5,9};
+
+
+        Arrays.sort(arr1); //for maintaining the ascending order of non-matching elements
+
+        List<Integer> list = new ArrayList<>();
+        Map<Integer,Integer> map = new LinkedHashMap<>();
+        for(int i=0;i<arr1.length;i++){
+            if(Arrays.toString(arr2).contains(String.valueOf(arr1[i]))){
+                map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);
+            }
+            else {
+                list.add(arr1[i]);
+            }
+        }
+        int index=0;
+        for(int i=0;i<arr2.length;i++){
+            if(map.containsKey(arr2[i])){
+                int count = map.get(arr2[i]);
+                while (count>0){
+                    list.add(index,arr2[i]);
+                    count--;
+                    index++;
+                }
+            }
+        }
+        list.forEach(e->System.out.print(e));
+       // map.forEach((k,v) -> System.out.println(k+","+v));
+    }
 }
--- src/main/java/org/example/FruitBasket.java
+++ src/main/java/org/example/FruitBasket.java
@@ -1,2 +1,47 @@
-package org.example;public class FruitBasket {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given we two arrays of fruit basket and its price, we need to buy max fruits with minimum price.
+ * int[] fruitBasket = {8, 6, 4, 7, 6, 5};
+ * int[] priceOfEachBasket = {8, 5, 5, 9, 2, 4};
+ * int maxFruits = 15;
+ */
+public class FruitBasket {
+    public static void main(String[] args) {
+        int[] fruitBasket = {8, 6, 4, 7, 6, 5};
+        int[] priceOfEachBasket = {8, 5, 5, 9, 2, 4};
+        int maxFruits = 6;
+
+        int totalFruit = 0;
+        int totalCost = 0;
+
+        Integer[] basketIndex = new Integer[fruitBasket.length];
+        for(int i=0;i<fruitBasket.length;i++){
+            basketIndex[i] =i;
+        }
+
+        List<Integer> bag = new ArrayList<>();
+
+        Arrays.sort(basketIndex,(a,b)->Integer.compare(priceOfEachBasket[a],priceOfEachBasket[b]));
+
+        for(int index:basketIndex){
+            if(totalFruit+fruitBasket[index]<=maxFruits){
+                totalFruit = totalFruit + fruitBasket[index];
+                totalCost = totalCost +priceOfEachBasket[index];
+                bag.add(fruitBasket[index]);
+            }
+            else {
+                break;
+            }
+        }
+
+        bag.forEach(b->System.out.println("Contains of bag:"+b));
+        System.out.println("Total Fruits:"+totalFruit);
+        System.out.println("Total Money Spent:"+totalCost);
+    }
 }
